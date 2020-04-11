d771c462326297a88ddc89f06234ab767048e24b
Bruno Luiz de Siqueira <brunoluizdesiqueira@gmail.com>
2020-04-11 17:00:30 -0300

    Altera rota para teste3

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/teste', (req, res) => {
-  return res.json({hello: 'Hello World 2'});
+app.get('/teste3', (req, res) => {
+  return res.json({hello: 'Hello World 3'});
 });
 
 app.listen(3333);
